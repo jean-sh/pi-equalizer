b40f1030de0a8f74fb7476ddc875b4c06b9d0e66
Jean Vincent <jean.vincent@posteo.net>
2017-01-28 09:42:53 +0100

    More work on extraction

--- audioExtraction.py
+++ audioExtraction.py
@@ -27,13 +27,11 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy as sp
 import wave
-from pyAudioAnalysis import audioBasicIO
-from pyAudioAnalysis import audioFeatureExtraction
 import struct
 
-def audio_spectrum(wav_file_path, nb_of_frames=2048, nb_of_points=128):
+
+def audio_spectrum(wav_file_path, nb_of_frames=2205, nb_of_points=128):
     """
     Extracts and plots the audio spectrum of a wav file from the first frames
     :param
@@ -49,10 +47,10 @@ def audio_spectrum(wav_file_path, nb_of_frames=2048, nb_of_points=128):
     """
 
     wav_file = wave.open(wav_file_path, 'r')
-    wav_file.readframes(nb_of_frames)
     data = wav_file.readframes(nb_of_frames)
     sampling_rate = wav_file.getframerate()
     nb_of_channels = wav_file.getnchannels()
+    print(wav_file.tell())
     wav_file.close()
 
     data = struct.unpack('{n}h'.format(n=nb_of_channels * nb_of_frames), data)
@@ -65,90 +63,93 @@ def audio_spectrum(wav_file_path, nb_of_frames=2048, nb_of_points=128):
     intensities = []
     for coef in fft_array:
         intensities.append(abs(coef))
+    print(intensities)
 
     # Calculate the frequencies associated
     freqs = np.fft.fftfreq(len(fft_array))
     freqs_in_hertz = []
     for freq in freqs:
         freqs_in_hertz.append(abs(freq * sampling_rate))
+    print(freqs_in_hertz)
 
     plt.plot(freqs_in_hertz, intensities)
-    plt.xscale("log")
-    plt.yscale("log")
+    #plt.xscale("log")
+    #plt.yscale("log")
     plt.show()
 
     return freqs_in_hertz, intensities
 
 
-audio_spectrum("stressmono3.wav", nb_of_points=32)
+# audio_spectrum("test.wav", nb_of_points=2048)
 
 
-def audio_to_ndarray(wavfile):
+def complete_audio_spectrum(wav_file_path, sample_size=2048, nb_of_points=2048):
     """
-    :param wavfile: the wav audio file to extract the MFCCs (Mel Frequency Cepstral Coefficients) from, must be mono
-    :return: a 2-dimensional numpy array, 13 columns and as many lines as frames
-    """
-    [Fs, x] = audioBasicIO.readAudioFile(wavfile)
-    features = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs)
-
-    return features[8:20]
-
-
-def extract_chroma_one_frame(frame):
-    """
-    Plots values from one frame as extracted by featureExtractionFile from pyAudioAnalysis
-    """
-    with open(frame, 'r') as f_frame:
-        features = f_frame.read()
-
-    values_str = features.split("\t")
-    list_values = []
-    for s in values_str:
-        list_values.append(float(s))
-
-    return list_values
-
-# extract_and_plot_one_frame('features')
-
-
-def extract_chroma_vector(data_file):
-    """
-    Extracts the chroma infos of an audio file over time
-    :param data_file: file containing chroma infos extracted by featureExtractionFile from pyAudioAnalysis,
-    the corresponding columns from the generated file are 22-33
-    :return numpy array consisting of all frames, each consisting of 12 values representing their spectral energy
+    Extracts the audio spectrum of a wav file
+    :param
+    wav_file_path:
+    The wav file to extract the spectrum from
+    nb_of_frames:
+    The number of frames from which to extract data at a time
+    nb_of_points:
+    The number of points to calculate the Fast Fourier Transform
+    :return
+    The array of frequencies
+    The array of corresponding intensities
     """
 
-    with open(data_file) as f:
-        data = f.read()
+    # Extract all useful data from the wav file
+    wav_file = wave.open(wav_file_path, 'r')
+    sampling_rate = wav_file.getframerate()
+    nb_of_channels = wav_file.getnchannels()
+    total_nb_of_frames = wav_file.getnframes()
+    data = []
+    while wav_file.tell() < total_nb_of_frames - sample_size:
+        d = wav_file.readframes(sample_size)
+        d = struct.unpack('{n}h'.format(n=nb_of_channels * sample_size), d)
+        d = np.array(d)
+        data.append(d)
+    wav_file.close()
 
-    str_all_frames = data.split("\n")
-    str_frame_list = []
-    for str_frame in str_all_frames:
-        str_frame_list.append(str_frame.split("\t"))
+    '''
+    # Get the Hanning window
+    w = np.hanning(nb_of_points)
+    # Apply the window
+    for d in data:
+        for j in range(len(w)):
+            d[j] = d[j] * w[j]
+    '''
 
-    float_frame_list = []
-    float_frame_values = []
-    for str_frame in str_frame_list:
-        for str_value in str_frame:
-            try:
-                float_frame_values.append(float(str_value))
-            except ValueError:
-                print(str_value)
-        float_frame_list.append(float_frame_values)
-        float_frame_values = []
+    # Calculate the Fourier Transform coefficients
+    fft_data = []
+    for d in data:
+        fft_data.append(np.fft.fft(d, nb_of_points))
+    # print(fft_data)
 
-    np_array_frames = np.array(float_frame_list)
-    return np_array_frames
+    # Calculate the power in each frequency
+    intensity_data = []
+    for f in fft_data:
+        f_intensity = []
+        for coef in f:
+            f_intensity.append(abs(coef))
+        intensity_data.append(f_intensity)
+    # print(intensity_data)
 
+    # Calculate the frequencies associated
+    freqs = np.fft.fftfreq(len(fft_data[0]))
+    # print(freqs)
 
-# extract_chroma_vector("song_features")
+    freqs_in_hertz = []
+    for freq in freqs:
+        freqs_in_hertz.append(abs(freq * sampling_rate))
+    # print(freqs_in_hertz)
+    for f in intensity_data:
+        plt.plot(freqs_in_hertz, f)
+        plt.ylim(0, 100000)
+        #plt.xscale("log")
+        #plt.yscale("symlog")
+        plt.show()
 
+    return freqs_in_hertz, intensity_data
 
-def plot_chroma_one_frame(chroma_values):
-    x = range(len(chroma_values))
-    plt.bar(x, chroma_values)
-    plt.show()
-
-
-# plot_chroma_one_frame(extract_chroma_one_frame("features"))
+complete_audio_spectrum("stressmono3.wav")
