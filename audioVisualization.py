d6509abb267a9cd8b73179e10f0d7646bca5d20f
Jean Vincent <jean.vincent@posteo.net>
2017-01-28 10:48:21 +0100

    Added a couple of functions

--- audioVisualization.py
+++ audioVisualization.py
@@ -25,16 +25,57 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import audioExtraction as aex
 import matplotlib.pyplot as plt
-import audioExtraction
 
 
-def plot_np_array(array):
-    i = 0
-    while i < 128:
-        x = range(len(array[i]))
-        plt.bar(x, array[i])
-        plt.show()
-        i += 16
+def linear_average(freq_data, intensity_data, nb_of_points=8):
+    length = len(freq_data)
+    size = length // nb_of_points
 
-plot_np_array(audioExtraction.extract_chroma_vector("song_features"))
+    freq_avg = []
+    for n in range(nb_of_points):
+        f = sum(freq_data[n*size:(n+1)*size]) // size
+        freq_avg.append(f)
+
+    inten_avg = []
+    for j, frame_intensities in enumerate(intensity_data):
+        frame_avg = []
+        for n in range(nb_of_points):
+            i = sum(frame_intensities[n*size:(n+1)*size]) // size
+            frame_avg.append(i)
+        inten_avg.append(frame_avg)
+    return freq_avg, inten_avg
+
+
+def bar_plot(freq_data, intensity_data):
+    plt.ylim(0, 256)
+    plt.bar(freq_data, intensity_data, 1000)
+    #plt.yscale("symlog")
+    plt.show()
+
+
+def rescale_intensity(intensity_data):
+    max_global = 0
+    for f in intensity_data:
+        max_in_f = max(f)
+        if max_in_f > max_global:
+            max_global = max_in_f
+
+    i_rescaled = []
+    for f in intensity_data:
+        f_rescaled = []
+        for i in f:
+            f_rescaled.append((i / max_global) * 200 + 55)
+        i_rescaled.append(f_rescaled)
+
+    return i_rescaled
+
+'''
+freq, intensity = aex.complete_audio_spectrum("stressmono3.wav")
+freq, intensity = linear_average(freq, intensity)
+intensity = rescale_intensity(intensity)
+
+for f in intensity:
+    bar_plot(freq, f)
+'''
