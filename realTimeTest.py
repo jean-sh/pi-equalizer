57c3cb4751c7a8ee988a337dc8c0d33e66c76d59
Jean Vincent <jean.vincent@posteo.net>
2017-01-29 15:04:58 +0100

    Added multithreaded code for processing in real time

--- realTimeTest.py
+++ realTimeTest.py
@@ -25,32 +25,35 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-
-##
-# Plays a wave file
-##
 import pyaudio
 import wave
 import numpy as np
-import matplotlib.pyplot as plt
 import time
 import struct
+import threading
+import queue
+import audioVisualization as avi
 
 
-wf = wave.open("heaven30.wav", 'r')
-
-# instantiate PyAudio
-p = pyaudio.PyAudio()
-
-
-# define callback
-def callback(in_data, frame_count, time_info, status):
-    data = wf.readframes(frame_count)
-    
-    nb_of_channels = wf.getnchannels()
-
-    nb_of_points = 128
-    data = struct.unpack('{n}h'.format(n=nb_of_channels * frame_count), data)
+def extract(data, frame_count, nb_channels, frame_rate, q, nb_of_points=128):
+    """
+    Extracts and plots the audio spectrum of audio data
+    :param
+    data:
+    The audio data to extract the spectrum from
+    frame_count:
+    The number of frames corresponding to the data
+    nb_channels:
+    The number of channels of the audio data
+    frame_rate:
+    The frame rate of the audio data
+    nb_of_points:
+    The number of points to calculate the Fast Fourier Transform
+    :return
+    The array of frequencies
+    The array of corresponding intensities
+    """
+    data = struct.unpack('{n}h'.format(n=nb_channels * frame_count), data)
     data = np.array(data)
 
     # Calculate the Fourier Transform coefficients
@@ -60,31 +63,73 @@ def callback(in_data, frame_count, time_info, status):
     intensities = []
     for coef in fft_array:
         intensities.append(abs(coef))
+    #print(intensities)
 
     # Calculate the frequencies associated
     freqs = np.fft.fftfreq(len(fft_array))
+    freqs_in_hertz = []
+    for freq in freqs:
+        freqs_in_hertz.append(abs(freq * frame_rate))
+    #print(freqs_in_hertz)
 
-    return (data, pyaudio.paContinue)
+    q.put(freqs_in_hertz)
+    q.put(intensities)
 
-# open stream using callback
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True,
-                stream_callback=callback)
 
-# start the stream
-stream.start_stream()
+##
+# Plays a wave file and extracts the audio spectrum at the same time
+##
+def play_and_extract(wav_file):
+    wf = wave.open(wav_file, 'r')
+    nb_channels = wf.getnchannels()
+    frame_rate = wf.getframerate()
 
-# wait for stream to finish
-while stream.is_active():
-    time.sleep(0.1)
+    # Create a queue for the extract thread and the display thread
+    q = queue.Queue()
 
-# stop stream
-stream.stop_stream()
-stream.close()
-wf.close()
+    # instantiate PyAudio
+    pyau = pyaudio.PyAudio()
 
-# close PyAudio
-p.terminate()
+    # define callback
+    def callback(in_data, frame_count, time_info, status):
+        data = wf.readframes(frame_count)
 
+        # Create the extract thread
+        extract_thread = threading.Thread(target=extract,
+                                          args=(data, frame_count, nb_channels, frame_rate, q, 512))
+        extract_thread.start()
+
+        # Block until extracting is done and queue is empty
+        extract_thread.join()
+        freqs = q.get()
+        intensities = q.get()
+
+        # Process the extracted data for displaying
+        freqs, intensities = avi.avg_custom_range_one_chunk(freqs, intensities)
+        intensities = avi.rescale_intensity(intensities)
+        print(freqs, intensities)
+        return data, pyaudio.paContinue
+
+    # open stream using callback
+    stream = pyau.open(format=pyau.get_format_from_width(wf.getsampwidth()),
+                       channels=nb_channels,
+                       rate=frame_rate,
+                       output=True,
+                       stream_callback=callback)
+
+    # start the stream
+    stream.start_stream()
+
+    # wait for stream to finish
+    while stream.is_active():
+        time.sleep(1)
+
+    # stop stream
+    stream.stop_stream()
+    stream.close()
+    wf.close()
+
+    # close PyAudio
+    pyau.terminate()
+
+play_and_extract("stress.wav")
