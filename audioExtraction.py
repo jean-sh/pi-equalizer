3ace754d924927e50f165b6c81e981b9d12c9e98
Jean Vincent <jean.vincent@posteo.net>
2017-02-03 19:13:29 +0100

    Restructuring

--- audioExtraction.py
+++ audioExtraction.py
@@ -25,126 +25,52 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import matplotlib.pyplot as plt
 import numpy as np
-
-import wave
+import cv2
 import struct
+import math
 
 
-def audio_spectrum(wav_file_path, nb_of_frames=2205, nb_of_points=128):
+def calculate_frequency_ranges(data, frame_count, frame_rate, nb_channels):
     """
-    Extracts and plots the audio spectrum of a wav file from the first frames
-    :param
-    wav_file_path:
-    The wav file to extract the spectrum from
-    nb_of_frames:
-    The number of frames to extract the spectrum from
-    nb_of_points:
-    The number of points to calculate the Fast Fourier Transform
-    :return
-    The array of frequencies
-    The array of corresponding intensities
+    TODO
     """
-
-    wav_file = wave.open(wav_file_path, 'r')
-    # wav_file.readframes(50000)
-    data = wav_file.readframes(nb_of_frames)
-    sampling_rate = wav_file.getframerate()
-    nb_of_channels = wav_file.getnchannels()
-
-    wav_file.close()
-
-    data = struct.unpack('{n}h'.format(n=nb_of_channels * nb_of_frames), data)
-    data = np.array(data)
-
+    
+    data = np.array(struct.unpack('{n}h'.format(n=nb_channels * frame_count), data))
     # Calculate the Fourier Transform coefficients
-    fft_array = np.fft.fft(data, nb_of_points)
-
-    # Calculate the power in each frequency
-    intensities = []
-    for coef in fft_array:
-        intensities.append(abs(coef))
+    dft_array = cv2.dft(np.float32(data))
 
     # Calculate the frequencies associated
-    freqs = np.fft.fftfreq(len(fft_array))
-    freqs_in_hertz = []
+    freqs = np.fft.fftfreq(len(dft_array))
+    freqs_hertz = []
     for freq in freqs:
-        freqs_in_hertz.append(abs(freq * sampling_rate))
-    return freqs_in_hertz, intensities
+        freqs_hertz.append(abs(freq * frame_rate))
+    
+    # TODO
+    length = len(freqs_hertz)
+    custom_freqs = [58, 130, 292, 657, 1478, 3325, 7482, 16834]
+    
+    # Calculate frequency boundaries
+    boundaries = []
+    for i in range(len(custom_freqs) - 1):
+        # Using the geometric mean seems better than arithmetic mean here
+        b = math.sqrt((custom_freqs[i] * custom_freqs[i+1]))
+        boundaries.append(b)
+    boundaries.append(22050)
+
+    return freqs_hertz, boundaries
 
 
-# audio_spectrum("test.wav", nb_of_points=2048)
-
-
-def complete_audio_spectrum(wav_file_path, sample_size=2048, nb_of_points=2048):
+def calculate_magnitudes(data, frame_count, nb_channels):
     """
-    Extracts the audio spectrum of a wav file
-    :param
-    wav_file_path:
-    The wav file to extract the spectrum from
-    nb_of_frames:
-    The number of frames from which to extract data at a time
-    nb_of_points:
-    The number of points to calculate the Fast Fourier Transform
-    :return
-    The array of frequencies
-    The array of corresponding intensities
+    TODO
     """
-
-    # Extract all useful data from the wav file
-    wav_file = wave.open(wav_file_path, 'r')
-    sampling_rate = wav_file.getframerate()
-    nb_of_channels = wav_file.getnchannels()
-    total_nb_of_frames = wav_file.getnframes()
-    data = []
-    while wav_file.tell() < total_nb_of_frames - sample_size:
-        d = wav_file.readframes(sample_size)
-        d = struct.unpack('{n}h'.format(n=nb_of_channels * sample_size), d)
-        d = np.array(d)
-        data.append(d)
-    wav_file.close()
-
-    '''
-    # Get the Hanning window
-    w = np.hanning(nb_of_points)
-    # Apply the window
-    for d in data:
-        for j in range(len(w)):
-            d[j] = d[j] * w[j]
-    '''
-
+    data = np.array(struct.unpack('{n}h'.format(n=nb_channels * frame_count), data))
+    windowed_data = np.multiply(data, np.hanning(len(data)))
     # Calculate the Fourier Transform coefficients
-    fft_data = []
-    for d in data:
-        fft_data.append(np.fft.fft(d, nb_of_points))
-    # print(fft_data)
+    dft_array = cv2.dft(np.float32(windowed_data))
+    # Return the power in each frequency
+    magnitudes = np.add(np.sqrt((dft_array*dft_array).sum(axis=1)), 10)
+    log_mag = np.log10(magnitudes)
 
-    # Calculate the power in each frequency
-    intensity_data = []
-    for f in fft_data:
-        f_intensity = []
-        for coef in f:
-            f_intensity.append(abs(coef))
-        intensity_data.append(f_intensity)
-    # print(intensity_data)
-
-    # Calculate the frequencies associated
-    freqs = np.fft.fftfreq(len(fft_data[0]))
-    # print(freqs)
-
-    freqs_in_hertz = []
-    for freq in freqs:
-        freqs_in_hertz.append(abs(freq * sampling_rate))
-    # print(freqs_in_hertz)
-    '''
-    for f in intensity_data:
-        plt.plot(freqs_in_hertz, f)
-        plt.ylim(0, 100000)
-        #plt.xscale("log")
-        #plt.yscale("symlog")
-        plt.show()
-    '''
-    return freqs_in_hertz, intensity_data
-
-#complete_audio_spectrum("stressmono3.wav")
+    return log_mag
